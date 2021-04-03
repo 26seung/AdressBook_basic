ef2c5dffee9a0e1fdb430ac1ce68ea1e95f30c84
LEEYUSEUNG <LEEYUSEUNG@220.85.164.169>
2021-04-03 23:49:35 +0900

    final

--- AddressBook/src/io/BookControllar.java
+++ AddressBook/src/io/BookControllar.java
@@ -7,6 +7,7 @@ import java.io.FileWriter;
 import java.io.Reader;
 import java.io.Writer;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Scanner;
 
@@ -31,7 +32,7 @@ public class BookControllar {
 					continue;
 				case 2: //등록
 					view.addView();
-					addData();
+					addFile();
 					continue;
 				case 3: //삭제
 					view.delView();
@@ -62,12 +63,14 @@ public class BookControllar {
 			Reader rd = new FileReader("files\\data.txt");
 			BufferedReader br = new BufferedReader(rd);
 			String line = null;
-			
-			while((line=br.readLine()) != null) {
-				BookData data = new BookData();		// 이해안간다 위에는 적용안됨
+			BookData data = null;
+
+			while((line=br.readLine()) != null) {	
+//				BookData data = new BookData();				 이해안간다 위에는 적용안됨
+				data = new BookData();						// 이런 식으로 하면 적용 가능
 				String[] dataArray = new String[3];
 				dataArray = line.split(",");
-				if (dataArray.length != 3) continue;
+				if (dataArray.length != 3) continue;	// 왜 넣어야 하는지 변수생각해보자.
 				
 				data.setName(dataArray[0]);
 				data.setHp(dataArray[1]);
@@ -93,32 +96,13 @@ public class BookControllar {
 			System.out.print(data.get(i).getName()+"\t");
 			System.out.print(data.get(i).getHp()+"\t");
 			System.out.print(data.get(i).getTel()+"\n");
-		}
+		}	System.out.println("");
+			System.out.println("    ----- [검색 완료] -----");
 	}
 	
 		///////////////////////////////////////////////////
-			
-	public void insertFile(BookData data) {
-		
-		try {
-//			BookData data = new BookData();
-			Writer fw = new FileWriter("files\\data.txt",true);
-			BufferedWriter bw = new BufferedWriter(fw);
-			
-			
-			bw.append(data.getName() + data.getHp() + data.getTel());
-				
-			bw.flush();
-			bw.close();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
-	}
 	
 	public BookData addData() {
-		
-//		List<BookData>list = new ArrayList<>();
-		BookData data = new BookData();
 		
 		System.out.print(">이름: ");
 		String name = sc.next();
@@ -127,19 +111,65 @@ public class BookControllar {
 		System.out.print(">집전화: ");
 		String tel = sc.next();
 		
-		data.setName(name);
-		data.setHp(hp);
-		data.setTel(tel);
+		BookData data = new BookData(name,hp,tel);
+		
 		return data;
-		//list.add(new BookData(name,hp,tel));
-				
 	}
+
+	public void addFile() {
+		
+		try {
+			BookData data = addData();
+			
+			Writer fw = new FileWriter("files\\data.txt",true);
+			BufferedWriter bw = new BufferedWriter(fw);
+			
+			String str;
+			str = data.getName() + "," + data.getHp() + "," + data.getTel();
+			
+			
+			bw.newLine();
+			bw.append(str);
+//			bw.write(str);			// ( append or write ) 차이점은?
+			bw.flush();
+			bw.close();
+		} catch(Exception e) {
+			e.printStackTrace();
+		} 	System.out.println("");
+			System.out.println("    ----- [등록 완료] -----");
+	}
+		
 	
 	///////////////////////////////////////////////////
 	
-	public void delData() {				
-		List<BookData>list = getFile();	
-		list.remove(sc.nextInt() -1);
+	public void delData() {		
+		
+		List<BookData>data = getFile();
+		
+		try {
+		Writer fw = new FileWriter("files\\data.txt");
+		BufferedWriter bw = new BufferedWriter(fw);
+		
+		String str = null;
+		data.remove(sc.nextInt()-1);
+		for (int i = 0; i < data.size(); i++) {
+			str = data.get(i).getName() + "," + data.get(i).getHp() + "," + data.get(i).getTel() + "\n";
+			
+//			bw.newLine();				//  "\n" 대신 사용하면 데이터가 한칸 아래에 파일 저장됨
+			bw.write(str);
+		}	
+		
+		bw.flush();
+		bw.close();
+		
+		}  catch(Exception e) {
+			e.printStackTrace();
+		}	System.out.println("");
+			System.out.println("    ----- [삭제 완료] -----");
+			System.out.println("");
+			listData();
+		
+
 	}
 	
 	///////////////////////////////////////////////////
@@ -156,8 +186,8 @@ public class BookControllar {
 				System.out.print(list.get(i).getHp()+"\t");
 				System.out.print(list.get(i).getTel()+"\n");
 			}
-		}
-		System.out.println("[검색 완료]");
+		}	System.out.println("");
+			System.out.println("    ----- [검색 완료] -----");
 	}
 	
 	///////////////////////////////////////////////////
